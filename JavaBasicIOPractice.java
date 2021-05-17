98de945ede292b83e23928d20d21e16554223d64
DonggeonMoon <83414846+DonggeonMoon@users.noreply.github.com>
2021-05-17 10:28:16 +0900

    Create JavaBasicIOPractice.java
    이름 입력 후,  커피 주문 여부를 'Y' 또는 'N'으로 입력받고, 'Y'일 경우 수량을 확인한 뒤 주문한 커피 수량을 반환하는 프로그램입니다. 
    Get a name first, and get if you order a cup of coffee with 'Y' or 'N'. If it is 'Y', after checking the number of cups, then return it.

---
+++ JavaBasicIOPractice.java
@@ -0,0 +1,38 @@
+package Day6;
+
+import java.util.Scanner;
+
+public class practice {
+	public static void main(String[] args) {
+		while (true) {
+			System.out.println("이름을 입력하세요");
+			Scanner sc1 = new Scanner(System.in);
+			String name = sc1.next();
+			System.out.println(name + "님 환영합니다.");
+			System.out.println("커피를 주문하시겠습니까?(Y/N)");
+			Scanner sc2 = new Scanner(System.in);
+			char answer = sc2.next().charAt(0);
+			while (true) {
+				if (answer == 'Y'){
+					System.out.println("수량을 입력하세요");
+					Scanner sc3 = new Scanner(System.in);
+					try {
+						int i1 = sc3.nextInt();
+						System.out.println(name + "님, 주문하신 커피 " + i1 + "잔 나왔습니다. 다음 손님 들어오세요!" );
+						//sc3.close();
+						break;
+					} catch (Exception e) {
+						System.out.println("숫자만 입력하셔야 합니다.("+ e + ")");
+						continue;
+					}
+				} else if(answer == 'N') {
+					System.out.println(name + "님, 안녕히 가십시오");
+					break;
+				} else {
+					System.out.println("잘못입력하셨습니다. 'Y' 또는 'N'을 입력해주세요");
+					answer = sc2.next().charAt(0);
+				}
+			}
+		}
+	}
+}
