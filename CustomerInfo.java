b998350d862fc8e1d55495ca280e142016097df7
DonggeonMoon <83414846+DonggeonMoon@users.noreply.github.com>
2021-05-26 21:26:38 +0900

    Create CustomerInfo.java

---
+++ CustomerInfo.java
@@ -0,0 +1,33 @@
+package cms;
+
+public class CustomerInfo {
+	private String name;
+	private char gender;
+	private String email;
+	private int birthYear;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getBirthYear() {
+		return birthYear;
+	}
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}	
+}
