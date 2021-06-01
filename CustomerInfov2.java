b53683307c31aadfe5d7ac3f15c7603867d4a4a5
DonggeonMoon <83414846+DonggeonMoon@users.noreply.github.com>
2021-06-01 17:40:22 +0900

    Create CustomerInfov2.java

---
+++ CustomerInfov2.java
@@ -0,0 +1,35 @@
+package cms;
+
+import java.io.Serializable;
+
+public class CustomerInfo implements Serializable {
+	private String name;//이름
+	private char gender;//성별
+	private String email;//이메일
+	private int birthYear;//태어난 연도
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getBirthYear() {
+		return birthYear;
+	}
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}	
+}
